9b2ba43eac1e0449865f53b49ef78216817c9c16
Ganti7 <soso1350@hotmail.fr>
2017-03-23 17:51:07 +0100

    Add files via upload

--- backjumping.c
+++ backjumping.c
@@ -1,80 +1,89 @@
 int BT(CSP * csp){
 
-int i;
-int continue=0;
-int x,v,y,x_val,y_val;
-
-int d[csp->var_length][csp->val_length];
-int tab[csp->var_length];
-
-if(var[0]==NULL)
-    return(true);
+int i,j;
+int bj=0;
 
 
-else
+int d[csp->var_length]={0};
+int tab[csp->val_length];
+int modif[csp->var_length]={0};
+
+int **tuples=NULL;
+
+
+
+
+
+for(i=0;i<csp->var_length-1=;i++)  // on choisit la variable
 {
+	if(modif[i]==1)   // tester plutot si on a deja fait un domaine
 
-    for(i=0;i<csp->var_length-1=;i++)
+	for(j=0;j<csp->val_length;j++) // on choisit une valeur
     {
-        //x=csp->variables[i];
-        d[i][0]=csp->variables[i]
-
-
-
-        for(int j=0;j<csp->val_length;j++)
+		
+		if(csp->domaines[i][j]==1) // si la valeur appartient au domaine
         {
-            if(csp->domaines[i][j]==1)
+            d[i]=csp->valeurs[i];   // assignation
+			for(int k=0;k<i;k++) // on parcourt les var deja assignées
             {
-               // x_val=csp->valeurs[j];
-
-                for(int k=0;k<csp->var_length;k++)
+				if(csp->contraintes[i][k]!=NULL) // si il y a une contrainte on verifie qu'elle ne soit pas enfreinte
                 {
-                    if(csp->contraintes[x][k]==1)
-                    {
-                        remonte=k;
-                        continue=1;
+					tuples=csp->contraintes[i][k];
+					if(tuples[i][d[k]])     // si tuple valide
+					{
+						bj=0;
+						continue;
+					}
+					else   // sinon on prépare le backjump
+					{
+                        remonte=k;   // remonte prend la valeur de la variable qui partage la contrainte violée
+                        bj=1;        // bj nous indique que l'on ne peut pas assigner une autre valeur pour l'instant
                         break;
-                    }
+					}
 
-                    continue=0;
-                    d[i][j]=csp->valeurs[j]
-                }
-            }
-            if(continue==0)
-            {
-                tab[i]=j
-                break;
-            }
-
-
-        }
-
-        if(continue==1)
+                //bj=0;
+                //d[i][j]=csp->valeurs[j]
+				}
+			}
+        if(bj==0)
         {
-            i=remonte-1;
-            j=tab[remonte];
-
+			tab[i]=j   // tab stocke la case du tableau de valeurs que l'on a choisit d'assigner
+            break;
         }
 
+
+		}
+
+    /*if(bj==1)
+    {
+		i=remonte-1;
+        j=tab[remonte];
+
+    }*/
+
+
+	}
+	if(bj==1)
+    {
+		i=remonte-1;
+        j=tab[remonte];
+        modif[i]=1;
 
     }
-
-
-
-
-
-
-
-
-
+	
 
 }
 
+for(int n=0 ; n<csp->var_length ; n++)
+{
+	if(d[i]==0)
+	{
+		printf("inconsistant");
+		return (0);
+	}
+}
 
-
-
-
-
-
+printf("consistant");
+return (1);
 
 }
