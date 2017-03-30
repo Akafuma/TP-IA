97c8c3ca1d8b7706e6cacd8dec130beec4aca8ff
Ganti7 <soso1350@hotmail.fr>
2017-03-31 01:15:20 +0200

    Add files via upload

--- backjumping.c
+++ backjumping.c
@@ -1,4 +1,3 @@
-
 #include "backjumping.h"
 
 // renvoie la valeur qui partage la contrainte ou -1
@@ -28,39 +27,40 @@ int backjumping(CSP * csp)
 {
     int EMPILE;
     int nb_sol = 0;
-
+    int DOMAINE;
     Pile p;
     init_pile(csp->var_length, &p);
     int temp_domaines[VARIABLE_MAX][VALEUR_MAX];
     domaines_copie(temp_domaines, csp->domaines, csp);
-    int contrainte_enfreinte = -1;  // pour connaitre la contrainte enfreinte courante
+    int contrainte_enfreinte;  // pour connaitre la contrainte enfreinte courante
     int tab_contraintes[csp->var_length];  // tabl qui garde en mémoire la variable la plus profonde qui partage la contrainte avec la variable courante
 
     for(int i=0;i<csp->var_length;i++)
-        tab_contraintes[i]=VARIABLE_MAX+1;
+        tab_contraintes[i]=-1;
+       // tab_contraintes[i]=VARIABLE_MAX+1;
 
     for(int num_var=0;num_var< csp->var_length;num_var++)  // on choisit la variable
     {
-		
+
         EMPILE = 0;
 
         if(!domaine_var_vide(temp_domaines, num_var))
         {
-			
+
             for(int num_val = 0; num_val < csp->val_length; num_val++)
             {
 
                 if(appartient_domaine(num_var, num_val, temp_domaines))
                 {
 
-                    csp->num_val_assigne[num_var] = num_val;                    
+                    csp->num_val_assigne[num_var] = num_val;
 
                     contrainte_enfreinte = assignation_enfreint_contraintes_bj(num_var, num_val, csp); // on stocke la variable qui enfreint ou non une contraine
-                    if(contrainte_enfreinte < tab_contraintes[num_var]  && (contrainte_enfreinte != -1))
+                    if(contrainte_enfreinte > tab_contraintes[num_var])
 						tab_contraintes[num_var]=contrainte_enfreinte;
-                    
+
                     printf("On assigne %d a la variable%d\n",num_val, num_var);
-                    
+
                     temp_domaines[num_var][num_val] = 0;
 
                     if(contrainte_enfreinte!=-1)
@@ -75,6 +75,7 @@ int backjumping(CSP * csp)
                     else // on peut assigner la var
                     {
                         empile(num_var, num_val, &p);
+
                        // printf("c'est ok\n");
 
                         if(pile_pleine(&p)) // on a trouvé une solution on cherche une autre valeur pour cette variable
@@ -82,12 +83,14 @@ int backjumping(CSP * csp)
                             nb_sol++;
                             printf("sol trouve\n");
                             depile(&p);
-                        
+
+                            if(domaine_var_vide(temp_domaines, num_var))
+                                DOMAINE=0;
                             continue;
                         }
 
                         EMPILE =1;
-                        
+
 
                         break;
                     }
@@ -96,9 +99,30 @@ int backjumping(CSP * csp)
             } // for val
         }
 
+       /* else
+            tab_contraintes[num_var]--;*/
+
         if(!EMPILE)
-        {          
-                contrainte_enfreinte=tab_contraintes[num_var];
+        {
+
+               contrainte_enfreinte=tab_contraintes[num_var];
+                if(DOMAINE==0){
+                    Etat * e = depile(&p);
+                    if(e == NULL)// On a dépilé une pile vide
+                        return nb_sol;
+
+                    reinitialiser_domaine(temp_domaines, num_var, csp->domaines);
+                    tab_contraintes[num_var]=-1;
+                    num_var = num_var - 2;
+
+                    DOMAINE=1;
+                    continue;}
+                if(domaine_var_vide(temp_domaines, contrainte_enfreinte))
+                    contrainte_enfreinte--;
+
+
+
+
                 for(;num_var>contrainte_enfreinte;num_var--)
                 {
 
@@ -106,15 +130,16 @@ int backjumping(CSP * csp)
                     if(e == NULL)
                         return nb_sol;
                     reinitialiser_domaine(temp_domaines,num_var, csp->domaines);
-                    tab_contraintes[num_var]=VARIABLE_MAX+1;
-                    printf("reinit de %d\n",num_var);
+                    tab_contraintes[num_var]=-1;
+                    printf("reinit de %d 0 vaut %d\n",num_var,csp->num_val_assigne[0]);
 
                 }
 
 
                 num_var = contrainte_enfreinte-1;
 
-            
+                //contrainte_enfreinte=-1;
+
             continue;
 
 
@@ -124,5 +149,3 @@ int backjumping(CSP * csp)
 
     return nb_sol;
 }
-
-
